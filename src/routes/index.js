5ea4d787e037fdcd637c3c7199b09a54495e503a
Marlom <falconsoft.3d@gmail.com>
2021-02-28 00:14:00 +0100

    sc

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,7 +5,9 @@ const port = process.env.PORT;
 // const stripe = require(process.env.STRIPE_PUBLIC_KEY)(process.env.STRIPE_PRIVATE_KEY);
 
 router.get('/', (req, res) =>{
-    res.render('index');
+    res.render('index', {
+            title: process.env.APPNAME}
+            );
 });
 
 module.exports = router;
